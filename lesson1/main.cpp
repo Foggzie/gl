a34cda286c0a946a845ee6173ef6a4cd085050d4
Gunther Fox <gunther.fox@gmail.com>
2014-08-13 14:38:50 -0700

    Finished Lesson 1

--- lesson1/main.cpp
+++ lesson1/main.cpp
@@ -1,6 +1,28 @@
 // Put GL includes
 
+#include <GL/glut.h>
+
+void RenderSceneCB();
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
+    
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    glutInitWindowSize(800, 600);
+    // glutInitWindowPosition(x, y);
+    glutCreateWindow("Tutorial 1");
+
+    glutDisplayFunc(RenderSceneCB);
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glutMainLoop();
+}
+
+void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
 }
